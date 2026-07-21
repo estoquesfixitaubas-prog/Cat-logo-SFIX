f3ce0a75c7f37226ef6e290a81ca4cba57a1d823
CatalogoSfix <estoquesfixitaubas@gmail.com>
2026-07-21 14:58:54 -0400

    Atualiza catálogo (sw.js)

---
+++ catalogo-pwa/sw.js
@@ -0,0 +1,48 @@
+// ==CACHE-INICIO==
+const CACHE_NAME = 'catalogo-cache-v1';
+
+const SHELL_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./assets/logo-sfix.png",
+  "./assets/logo-makita.png",
+  "./assets/watermark-tile.png",
+  "./assets/seta-voltar.png",
+  "./assets/intro-makita.jpg",
+  "./assets/intro-dewalt.jpg",
+  "./assets/produtos/1-m9510b.png",
+  "./assets/produtos/2-m0920b.png",
+  "./assets/produtos/3-ga9020.png",
+  "./assets/produtos/4-dga517z.png",
+  "./assets/produtos/5-dga504z.png"
+];
+// ==CACHE-FIM==
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
